7b58fad931299462171779a3a16e332413b06b9c
Evandro Gaspar Ribeiro <evandrogrm@hotmail.com>
2021-05-31 22:45:34 -0300

    Added services api template

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import api from '../../services/api';
 import { Title } from './styles';
 
+interface Example {
+  id: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [temps, setTemps] = useState<Example[]>([]);
+
+  async function getExamples(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Example>(`examples/${temps}`);
+    const example = response.data;
+    setTemps([...temps, example]);
+  }
+
   return <Title>Dashboard</Title>;
 };
 
